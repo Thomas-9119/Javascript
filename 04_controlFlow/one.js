3e78fbeed386a67d12be88deb3b7b37c0c302fc9
Thomas-9119 <dharrajparno@gmail.com>
2026-01-02 18:07:04 +0530

    Initial

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,11 @@
+const score = 200
+if(score>100){
+  const power = "fly"
+  console.log(`User power:${power}`)
+}
+
+
+const balance = 1000
+
+if(balance>500) console.log("test"),
+console.log("test 2")
